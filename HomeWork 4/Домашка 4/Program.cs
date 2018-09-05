4c3ddcb6c02cc7846dd53d5409b740309e53f4bb
XcenaX <40425017+XcenaX@users.noreply.github.com>
2018-09-05 22:16:18 +0600

    Add files via upload

---
+++ HomeWork 4/Домашка 4/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Train train = new Train();
+            train.AddVagon(34, 25);
+            train.AddVagon(12, 41);
+            train.ShowInfoAboutVagons();
+
+
+            Console.WriteLine("\nПока только один вагон, добавьте еще");
+            int number = 0;
+            bool checkError = false;
+            int countOfPeople = 0;
+
+            while (!checkError)
+            {
+                Console.WriteLine("Введите кол-во пассажиров: ");
+                string toParse = Console.ReadLine();
+
+                if (int.TryParse(toParse, out countOfPeople) == false)
+                {
+                    checkError = true;
+                    Console.Clear();
+                    Console.WriteLine("Вы ввели не число!");
+                }
+
+
+                if (!checkError)
+                {
+                    Console.WriteLine("Введите номер вагона: ");
+                    toParse = Console.ReadLine();
+
+                    if (int.TryParse(toParse, out number) == false)
+                    {
+                        checkError = true;
+                        Console.Clear();
+                        Console.WriteLine("Вы ввели не число!");
+                    }
+                }
+
+                if (!checkError)
+                {
+                    try
+                    {
+                        train.AddVagon(number, countOfPeople);
+                    }
+                    catch (Exception)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Вагон с таким номером уже существует!");
+                        checkError = true;
+                    }
+                    if (!checkError) break;
+                }
+                checkError = false;
+            }
+            Console.Clear();
+
+
+
+            int number2 = 0;
+            checkError = false;
+
+            while (!checkError)
+            {
+                train.ShowInfoAboutVagons();
+                Console.WriteLine("\nТеперь попробуйте удалить вагон");
+                Console.WriteLine("просто введите номер вагона: ");
+
+                string toParse = Console.ReadLine();
+
+                if (int.TryParse(toParse, out number2) == false)
+                {
+                    checkError = true;
+                    Console.Clear();
+                    Console.WriteLine("Вы ввели не число!");
+                }
+
+                if (!checkError)
+                {
+                    try { train.DeleteVagon(number2); }
+                    catch (Exception)
+                    {
+                        checkError = true;
+                    }
+                }
+
+                if (!checkError)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Вагон успешно удален!");
+                    train.ShowInfoAboutVagons();
+                    break;
+                }
+                checkError = false;
+            }
+
+            Console.WriteLine("\nНажмите любую кнопку !!!");
+            Console.ReadKey();
+            Console.Clear();
+
+
+            Console.Write("Введите имя поезда: ");
+            train.SetName(Console.ReadLine());
+
+            double speed = 0;
+            checkError = false;
+
+            while (!checkError)
+            {
+                Console.Write("Введите скорость поезда: ");
+                string toParse = Console.ReadLine();
+
+                if(double.TryParse(toParse, out speed) == false)
+                {
+                    checkError = true;
+                    Console.Clear();
+                    Console.WriteLine("Вы ввели не число!");                
+                }
+
+                if (!checkError)
+                {
+                train.SetSpeed(speed);
+
+                Console.WriteLine("\nИнформация об этом поезде");
+                train.ShowInfoAboutTrain();
+                train.ShowInfoAboutVagons();
+
+                Console.ReadKey();
+                    break;
+                }
+                checkError = false;
+            }
+        }
+    }
+}
---
+++ HomeWork 4/Домашка 4/Train.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2 
+{
+    struct Vagon
+    {
+        private int number;
+        private int countOfPeople;
+
+        public void SetNumber(int number)
+        {
+            this.number = number;
+        }
+
+        public int GetNumber()
+        {
+            return number;
+        }
+
+        public void SetCountOfPeople( int countOfPeople)
+        {
+            this.countOfPeople = countOfPeople;
+        }
+
+        public int GetCountOfPeople()
+        {
+            return countOfPeople;
+        }
+    }
+
+    class Train
+    {
+       private Vagon[] vagon;
+        double speed;
+        string name;
+
+        public Train()
+        {
+            vagon = new Vagon[0];
+            name = "";
+            
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetSpeed(double speed)
+        {
+            this.speed = speed;
+        }
+
+        public double GetSpeed()
+        {
+            return speed;
+        }
+
+
+        public Object GetVagon(int number)
+        {
+            Object obj = number;
+            for (int i = 0; i < vagon.Length; i++)
+            {
+                if (obj == vagon.GetValue(i))
+                {
+                    return vagon.GetValue(i);
+                }
+            }
+            return false;
+        }
+        
+        public void SetNumberOfVagon(int oldNumberOfVagon, int newNumberOfVagon)
+        {
+            Object obj = oldNumberOfVagon;
+            for(int i = 0; i < vagon.Length; i++)
+            {
+                if(obj == vagon.GetValue(i))
+                {
+                    vagon.SetValue(newNumberOfVagon,i);
+                    return;
+                }
+            }
+            Console.WriteLine("Такого номера вагона не найдено!!");
+        }
+
+        public void AddVagon(int number, int countOfPeople)
+        {
+            for(int i = 0; i < vagon.Length; i++)
+            {                
+                if (vagon[i].GetNumber() == number) { throw new Exception(); }                                
+            }
+            Array.Resize(ref vagon, vagon.Length+1);
+            vagon[vagon.Length-1].SetCountOfPeople(countOfPeople);
+            vagon[vagon.Length-1].SetNumber(number);
+        }
+
+       public void DeleteVagon(int number)
+        {
+            int index = -1;
+            for (int i = 0; i < vagon.Length; i++)
+            {
+                if (vagon[i].GetNumber() == number) { index = i;break; }
+            }
+
+            if (index != -1)
+            {
+                for(int i = index;i< vagon.Length-1; i++)
+                {
+                vagon[i].SetCountOfPeople(vagon[i+1].GetCountOfPeople());
+                vagon[i].SetNumber(vagon[i + 1].GetNumber());
+                }
+                Array.Resize(ref vagon, vagon.Length-1);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Вагона с таким номером не найдено!");
+                throw new Exception();
+            }
+        }
+
+        public int GetCountfVagons()
+        {
+            return vagon.Length;
+        }
+
+        public void ShowInfoAboutVagons()
+        {
+            Console.WriteLine("\nКоличество вагонов: " + vagon.Length);
+            for (int i = 0; i < vagon.Length; i++)
+            {
+                Console.WriteLine("\nВагон " + (i+1) + ":");
+                Console.WriteLine("Номер вагона: " + vagon[i].GetNumber());
+                Console.WriteLine("Количество человек в вагоне: " + vagon[i].GetCountOfPeople());
+            }
+        }
+
+        public void ShowInfoAboutTrain()
+        {
+            Console.WriteLine("\nСкорость поезда: " + speed);
+            Console.WriteLine("Имя поезда: " + name);
+            Console.WriteLine("Количество вагонов: " + vagon.Length);
+            Console.WriteLine("Общее кол-во пассажиров: " + GetCountOfAllPeolpe());
+        }
+
+        public int GetCountOfAllPeolpe()
+        {
+            int sum = 0;
+            for (int i = 0; i < vagon.Length; i++)
+            {
+                sum += vagon[i].GetCountOfPeople();
+            }
+            return sum;
+        }
+
+    }
+}
