d286e646ade9017c166c1b985f5d6b5698977740
XcenaX <40425017+XcenaX@users.noreply.github.com>
2018-10-17 20:32:04 +0600

    Add files via upload

---
+++ WORK WITH FILES/HomeWork/HomeWork/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace HomeWork
+{
+    class Program
+    {
+
+        public static bool CheckBuffer(string[] buffer)
+        {
+            var copy = buffer;
+            int simvol;
+             
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if(!int.TryParse(buffer[i], out simvol))
+                {
+                    return false;
+                }
+            }
+            return true;
+        } 
+
+        public static bool AppendFebonachiNumbersToEndOfFile(string path)
+        {
+            try
+            {
+                string data;
+                string[] numbers;
+                using (StreamReader file = new StreamReader(path))
+                {
+                    data = file.ReadToEnd();
+                }
+                     numbers = data.Split(' ');
+                
+                    if (!CheckBuffer(numbers))
+                    {
+                        Clear();
+                        WriteLine("Ошибка : В вашем файле присутствуют не только цифры!");
+                        return false;
+                    }
+
+                    int oldLength = numbers.Length;
+
+                    Array.Resize(ref numbers, numbers.Length*2);
+                    
+                    for(int i =oldLength; i < numbers.Length; i++)
+                    {
+                        var sumStr = Convert.ToInt32(numbers[i - 1]) + Convert.ToInt32(numbers[i - 2]);
+                        numbers[i] = sumStr.ToString();
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(path))
+                    {
+                        for (int i = 0; i < numbers.Length; i++)
+                        {
+                            writer.Write(numbers[i] + " ");
+
+                        }
+                    }
+                    
+
+                    return true;
+                
+            }
+            catch (FileNotFoundException ex)
+            {
+                WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Write("Введите путь файла с числами Фебоначи: ");
+            string path = ReadLine();
+
+            AppendFebonachiNumbersToEndOfFile(path);
+
+        }
+    }
+}
---
+++ WORK WITH FILES/Practice/Practice/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using  System.IO;
+
+namespace Practice
+{
+    class Program
+    {
+
+        public static void PrintCountOfAllSimvolsInFile()
+        {
+
+            bool check = false;
+            string path;
+
+            while (!check)
+            {
+                Write("Введите путь файла в для подсчета всех символов: ");
+                path = ReadLine();
+
+                try
+                {
+                    using (StreamReader file = new StreamReader(path))
+                    {
+                        char[] buf = new char[file.BaseStream.Length];
+                        file.Read(buf, 0, Convert.ToInt32(file.BaseStream.Length));
+
+                        List<string> array = new List<string>();
+
+                        for (int i = 0; i < buf.Length; i++)
+                        {
+                            bool IsThere = false;
+
+                            for (int j = 0; j < array.Count; j++)
+                            {
+                                if (array[j].First() == buf[i])
+                                {
+                                    array[j] += buf[i];
+                                    IsThere = true;
+                                    break;
+                                }
+                            }
+
+                            if (!IsThere)
+                            {
+                                array.Add(buf[i].ToString());
+                            }
+
+                        }
+
+                        Clear();
+
+                        WriteLine("Символ\tКол-во");
+                        for (int i = 0; i < array.Count; i++)
+                        {
+                            WriteLine(array[i].First() + "\t" + array[i].Length);
+                        }
+                    }
+                    check = true;
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Clear();
+                    WriteLine(ex.Message);
+                    check = false;
+                }
+                catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }            
+        }
+
+        public static void WriteYourDataInFile()
+        {
+
+            bool check = false;
+            string path;
+
+            while (!check)
+            {
+                Write("Введите путь файла в для подсчета всех символов: ");
+                path = ReadLine();
+
+                try
+                {
+                    using (StreamWriter file = new StreamWriter(path))
+                    {
+                        WriteLine("Введите ваше Имя: ");
+                        string name = ReadLine();
+
+                        WriteLine("Введите вашу Фамилию: ");
+                        string lastName = ReadLine();
+
+                        int age;
+                        while (true)
+                        {
+                            WriteLine("Введите ваш Возраст: ");
+                            string toParse = ReadLine();
+
+                            if (!int.TryParse(toParse, out age))
+                            {
+                                Clear();
+                                WriteLine("Вы ввели не число!");
+                            }
+                            else break;
+                        }
+
+                        file.WriteLine(name);
+                        file.WriteLine(lastName);
+                        file.WriteLine(age);
+
+                    }
+                    check = true;
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Clear();
+                    WriteLine(ex.Message);
+                    check = false;
+                }
+                catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintCountOfAllSimvolsInFile();
+
+            WriteLine("\nНажмите любую кнопку . . .");
+            ReadKey();
+            Clear();
+
+            WriteYourDataInFile();
+
+            WriteLine("\nНажмите любую кнопку . . .");
+            ReadKey();
+            Clear();
+        }
+    }
+}
+
