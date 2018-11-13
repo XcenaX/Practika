1d0fbfbc7edf947439bf67e32c8bdf6c882c6079
XcenaX <40425017+XcenaX@users.noreply.github.com>
2018-11-13 17:21:44 +0600

    Add files via upload

---
+++ ADO NET Cteate Table/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            using (SqlConnection connection = new SqlConnection())
+            {
+                ConfigurationManager.AppSettings["isTest"].ToString();
+                string connectionString = ConfigurationManager.ConnectionStrings["UserConnectionString"].ConnectionString;
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                SqlCommand create = new SqlCommand();
+                create.CommandText = "create table gruppa(id int,name string );";
+
+            }
+        }
+    }
+}
